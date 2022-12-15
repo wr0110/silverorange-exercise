ab482fba526851b4db2becc024e153a9cd7609c1
wr0110 <wittyraccoon0110@gmail.com>
2022-12-15 13:56:12 +0100

    #TASK-B show markdown content

--- web/src/components/RepoDetailDialog.tsx
+++ web/src/components/RepoDetailDialog.tsx
@@ -7,14 +7,17 @@ import {
   Stack,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import { Repo } from '../models/Repo';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function RepoDetailDialog({
   repo: {
     name,
     updated_at: updatedAt,
     owner: { login },
+    full_name: fullName,
   },
   open,
   onClose,
@@ -23,6 +26,20 @@ function RepoDetailDialog({
   open: boolean;
   onClose: () => void;
 }) {
+  const [markDownContent, setMarkDownContent] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://raw.githubusercontent.com/${fullName}/master/README.md`)
+      .then((res) => res.json())
+      .then((content) => {
+        setMarkDownContent(content);
+      })
+      .catch(() => setMarkDownContent(null))
+      .finally(() => setIsLoading(false));
+  }, [fullName]);
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>{name}</DialogTitle>
@@ -33,6 +50,16 @@ function RepoDetailDialog({
           <Typography>
             Message: there's not approach to find commit hash value
           </Typography>
+          {isLoading ? (
+            <CircularProgress />
+          ) : markDownContent ? (
+            <>
+              <Typography align="center">MarkDown Content</Typography>
+              <ReactMarkdown>{markDownContent}</ReactMarkdown>
+            </>
+          ) : (
+            <Typography align="center">No README.md file</Typography>
+          )}
         </Stack>
       </DialogContent>
       <DialogActions>
