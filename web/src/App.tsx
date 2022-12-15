f1c498532f662656952fbbdd90a575b1cf7f1798
wr0110 <wittyraccoon0110@gmail.com>
2022-12-15 06:46:58 +0100

    #TASK-B fetch repository data

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
+import { Repo } from './models/Repo';
 
 export function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [repos, setRepos] = useState<Repo[]>([]);
+  console.log('repos: ', repos);
+
+  useEffect(() => {
+    fetch('/repos')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('data: ', data);
+        setRepos(data);
+      })
+      .catch(() => {
+        console.log('Error while fetching repositories');
+      });
+  }, []);
+
+  return <div>hi</div>;
 }
