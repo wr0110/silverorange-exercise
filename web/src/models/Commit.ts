19dd67d45e42a02ac33ae80f002471ff76fc29b8
wr0110 <wittyraccoon0110@gmail.com>
2022-12-15 06:46:01 +0100

    add proxy & interfaces for repos

---
+++ web/src/models/Commit.ts
@@ -0,0 +1,16 @@
+export interface Commit {
+  sha: string;
+  commit: {
+    message: string;
+    author: {
+      name: string;
+      email: string;
+      date: string;
+    };
+  };
+  author: {
+    login: string;
+    avatar_url: string;
+    url: string;
+  };
+}
