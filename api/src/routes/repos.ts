404907c9458934b405a2744626e3225cbac4851c
wr0110 <wittyraccoon0110@gmail.com>
2022-12-15 04:40:19 +0100

    #TASK-A dynamic import file and filter

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,19 @@
 import { Router, Request, Response, json } from 'express';
+import { Repo } from '../models/Repo';
 
 export const repos = Router();
 
 repos.get('/', json(), async (_: Request, res: Response) => {
+  const repositories: Repo[] = await (
+    await import('../../data/repos.json')
+  ).default;
+
+  const filteredRepos = repositories.filter(({ fork }) => fork === false);
+
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(filteredRepos);
 });
